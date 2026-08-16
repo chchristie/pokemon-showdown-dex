d80540eafa1f2677000597f23dfc54e666ec3aa5
Guangcong Luo <guangcongluo@gmail.com>
2026-08-16 11:26:15 -0700

    Validate IDs in tier and tag pages
    Thanks to mina for reporting this.

--- js/pokedex.js
+++ js/pokedex.js
@@ -388,6 +388,7 @@ var PokedexTagPanel = PokedexResultPanel.extend({
 		}
 	},
 	initialize: function(id) {
+		id = toID(id);
 		var tag = this.table[id];
 		var name = (tag ? tag.name : id);
 		this.id = id;
@@ -810,6 +811,7 @@ var PokedexCategoryPanel = PokedexResultPanel.extend({
 });
 var PokedexTierPanel = PokedexResultPanel.extend({
 	initialize: function(id) {
+		id = toID(id);
 		var tierTable = {
 			ag: "AG",
 			uber: "Uber",
